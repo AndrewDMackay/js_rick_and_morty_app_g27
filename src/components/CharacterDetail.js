75dfbd3b2e85d27c26496c22a0064938ed41f21a
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-17 17:32:20 +0100

    13th commit main

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -20,9 +20,9 @@ const CharacterDetail = ({selectedCharacter, onClickIndex, onClickFavourite}) =>
             <div className="detail-item">
                 <img src={selectedCharacter.image} alt={selectedCharacter.name}/>
                 <h2>{selectedCharacter.name}</h2>
-                <p>{selectedCharacter.status}</p>
-                <p>{selectedCharacter.species}</p>
-                <p>{selectedCharacter.type}</p>
+                <p>Status [ {selectedCharacter.status} ]</p>
+                <p>Species [ {selectedCharacter.species} ]</p>
+                <p>Gender [ {selectedCharacter.gender} ]</p>
                 <br></br>
                 <button onClick={handleFavourite}><strong>FAVOURITE..</strong></button>
                 <br></br>
@@ -35,18 +35,3 @@ const CharacterDetail = ({selectedCharacter, onClickIndex, onClickFavourite}) =>
 
 export default CharacterDetail;
 
-
-
-// Key	Type	Description
-// id	int	The id of the character.
-// name	string	The name of the character.
-// status	string	The status of the character ('Alive', 'Dead' or 'unknown').
-// species	string	The species of the character.
-// type	string	The type or subspecies of the character.
-// gender	string	The gender of the character ('Female', 'Male', 'Genderless' or 'unknown').
-// origin	object	Name and link to the character's origin location.
-// location	object	Name and link to the character's last known location endpoint.
-// image	string (url)	Link to the character's image. All images are 300x300px and most are medium shots or portraits since they are intended to be used as avatars.
-// episode	array (urls)	List of episodes in which this character appeared.
-// url	string (url)	Link to the character's own URL endpoint.
-// created	string	Time at which the character was created in the database.
