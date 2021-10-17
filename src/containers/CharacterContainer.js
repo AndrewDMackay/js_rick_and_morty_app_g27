041997d7db45c69c39bb0a3a84d0277f534b025f
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-17 15:01:44 +0100

    3rd commit main, added initial container, and components

---
+++ src/containers/CharacterContainer.js
@@ -0,0 +1,44 @@
+
+import React, { useState, useEffect } from 'react';
+// import CharacterList from '../components/CharacterList';
+// import CharacterDetail from '../components/CharacterDetail';
+// import CharacterFavourites from '../components/CharacterFavourites';
+
+
+const CharacterContainer = () => {
+    const [characters, setCharacters] = useState([]);
+    const [selectedCharacter, setSelectedCharacter, setFavouriteCharacter] = useState(null);
+    const [favouriteCharacters, setFavouriteCharacters] = useState([]);
+
+    useEffect(() => {
+      getCharacters();
+    }, [])
+
+
+    const getCharacters = function(){
+        fetch('https://rickandmortyapi.com/api')
+        .then(res => res.json())
+        .then(characters => setCharacters(characters))
+    }
+
+
+    const onCharacterClick = function(character){
+        setSelectedCharacter(character);
+    }
+
+
+    const onClickIndex = function(){
+        setSelectedCharacter(null);
+    }
+
+ 
+    return (
+        <div className="main-container">
+            {!selectedCharacter ? <CharacterList characters={characters} onCharacterClick={onCharacterClick}/> : null}
+            {selectedCharacter ? <CharacterDetail selectedCharacter={selectedCharacter} onClickIndex={onClickIndex}/> : null}
+            {!selectedCharacter ? <CharacterFavourites characters={characters}/> : null}
+        </div>
+    )
+}
+
+export default CharacterContainer;
